44c6acaac3c6ba2bd68396f9764bcdb593255c06
Torvul <43212694+torvul@users.noreply.github.com>
2023-03-29 17:06:23 -0400

    3-29-23 at 17:05
    Some smaller codes changes I made to the location of the Usercreation.aspx file
    I added the storedprocedureclass.cs which will hold all the calls to the stored procedure from the code

---
+++ TermProject/Classes/StoredProceduresClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TermProject.Classes
+{
+    public static class StoredProceduresClass
+    {
+        public static void CreateUser(String username, String fullname, String email, String password, String usertype, String question1, String question2, String quetion3)
+        {
+            
+        }
+
+    }
+}
