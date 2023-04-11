f09f17328c65af73fda696cae903831dc7bf2895
Torvul <43212694+torvul@users.noreply.github.com>
2023-04-11 15:33:45 -0400

    Updating with encryption password

--- TermProject/UserCreateLogin/UserCreation.aspx.cs
+++ TermProject/UserCreateLogin/UserCreation.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using StoredProcedures;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,12 +16,18 @@ namespace TermProject
     public partial class UserCreation : System.Web.UI.Page
     {
         protected static int recoverykey = -1;
+        private Byte[] key = { 250, 101, 18, 76, 45, 135, 207, 118, 4, 171, 3, 168, 202, 241, 37, 199 };
+
+        private Byte[] vector = { 146, 64, 191, 111, 23, 3, 113, 119, 231, 121, 252, 112, 79, 32, 114, 156 };
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 Random q = new Random();
                 recoverykey = q.Next(10000);
+
+                // hide this later
+                lblWarning.Text = recoverykey.ToString();
             }
         }
 
@@ -39,11 +48,53 @@ namespace TermProject
                 String username = txtBoxUsername.Text;
                 String fullname = txtBoxFullName.Text;
                 String email = txtBoxEmail.Text;
-                String password = txtBoxPassword.Text;
                 String usertype = ddlUserType.SelectedValue.ToString();
                 String question1 = txtBoxSecurityQuestion1.Text;
                 String question2 = txtBoxSecurityQuestion2.Text;
                 String question3 = txtBoxSecurityQuestion3.Text;
+
+
+
+
+                // password encryption shit
+                UTF8Encoding encoder = new UTF8Encoding();
+                String password = txtBoxPassword.Text;
+                Byte[] textBytes;
+
+                textBytes = encoder.GetBytes(txtBoxPassword.Text);
+
+               // a memory stream used to store the encrypted data temporarily, and
+
+            // a crypto stream that performs the encryption algorithm.
+
+                RijndaelManaged rmEncryption = new RijndaelManaged();
+
+                MemoryStream myMemoryStream = new MemoryStream();
+
+                CryptoStream myEncryptionStream = new CryptoStream(myMemoryStream, rmEncryption.CreateEncryptor(key, vector), CryptoStreamMode.Write);
+                // Use the crypto stream to perform the encryption on the plain text byte array.
+
+                myEncryptionStream.Write(textBytes, 0, textBytes.Length);
+
+                myEncryptionStream.FlushFinalBlock();
+                // Retrieve the encrypted data from the memory stream, and write it to a separate byte array.
+
+                myMemoryStream.Position = 0;
+
+                Byte[] encryptedBytes = new Byte[myMemoryStream.Length];
+
+                myMemoryStream.Read(encryptedBytes, 0, encryptedBytes.Length);
+
+                // Close all the streams.
+
+                myEncryptionStream.Close();
+
+                myMemoryStream.Close();
+
+
+                password = Convert.ToBase64String(encryptedBytes);
+
+
 
                 StoredProceduresClass.CreateUser(username, fullname, email, password, usertype, question1, question2, question3);
                 lblWarning.Text = "Account Created";
