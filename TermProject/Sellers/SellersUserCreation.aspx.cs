a15542e49362827b3288e9d18af48c79c1254ed6
Jason H <73149814+AlphaXII@users.noreply.github.com>
2023-04-29 00:32:30 -0400

    Project Update

--- TermProject/Sellers/SellersUserCreation.aspx.cs
+++ TermProject/Sellers/SellersUserCreation.aspx.cs
@@ -20,31 +20,70 @@ namespace TermProject.Sellers
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            DBConnect objDB = new DBConnect();
-            SqlCommand objCommand = new SqlCommand();
+            if (Session["usertype"].ToString() == "RealEstateAgent")
+            {
+                HttpCookie cookie = Request.Cookies["LoginSave"];
 
-            objCommand.CommandType = CommandType.StoredProcedure;
-            objCommand.CommandText = "TP_AddSeller";
+                string sellerUser = "";
+                string sellerName = "";
+
+                if (cookie != null) 
+                {
+                    sellerUser = cookie.Values["username"];
+                    sellerName = cookie.Values["name"];
+                }
+
+                DBConnect objDB = new DBConnect();
+                SqlCommand objCommand = new SqlCommand();
+
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "TP_AddSellerbyAgent";
 
 
-            SqlParameter input = new SqlParameter("@username", Session["username"].ToString());
-            input.Direction = ParameterDirection.Input;
-            input.SqlDbType = SqlDbType.VarChar;
-            objCommand.Parameters.Add(input);
+                SqlParameter input = new SqlParameter("@username", sellerUser);
+                objCommand.Parameters.Add(input);
 
-            input = new SqlParameter("@age", txtAge.Text);
-            input.Direction = ParameterDirection.Input;
-            input.SqlDbType = SqlDbType.VarChar;
-            objCommand.Parameters.Add(input);
+                input = new SqlParameter("@agent", Session["username"].ToString());
+                objCommand.Parameters.Add(input);
 
-            input = new SqlParameter("@name", Session["name"].ToString());
-            input.Direction = ParameterDirection.Input;
-            input.SqlDbType = SqlDbType.VarChar;
-            objCommand.Parameters.Add(input);
+                input = new SqlParameter("@age", txtAge.Text);
+                objCommand.Parameters.Add(input);
 
-            objDB.DoUpdate(objCommand);
+                input = new SqlParameter("@name", sellerName);
+                objCommand.Parameters.Add(input);
 
-            Response.Redirect("SellerHomepage.aspx");
+                objDB.DoUpdate(objCommand);
+
+                Response.Redirect("../Agents/AgentHomepage.aspx");
+            }
+            else
+            {
+                DBConnect objDB = new DBConnect();
+                SqlCommand objCommand = new SqlCommand();
+
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "TP_AddSeller";
+
+
+                SqlParameter input = new SqlParameter("@username", Session["username"].ToString());
+                input.Direction = ParameterDirection.Input;
+                input.SqlDbType = SqlDbType.VarChar;
+                objCommand.Parameters.Add(input);
+
+                input = new SqlParameter("@age", txtAge.Text);
+                input.Direction = ParameterDirection.Input;
+                input.SqlDbType = SqlDbType.VarChar;
+                objCommand.Parameters.Add(input);
+
+                input = new SqlParameter("@name", Session["name"].ToString());
+                input.Direction = ParameterDirection.Input;
+                input.SqlDbType = SqlDbType.VarChar;
+                objCommand.Parameters.Add(input);
+
+                objDB.DoUpdate(objCommand);
+
+                Response.Redirect("SellerHomepage.aspx");
+            }
         }
     }
 }
--- TermProject/UserCreateLogin/AgentSellerCreation.aspx.cs
+++ TermProject/UserCreateLogin/AgentSellerCreation.aspx.cs
@@ -15,6 +15,8 @@ namespace TermProject.UserCreateLogin
 {
     public partial class AgentSellerCreation : System.Web.UI.Page
     {
+        protected static int recoverykey = -1;
+
         private string sha256(string randomString)
         {
             var crypt = new SHA256Managed();
@@ -29,11 +31,56 @@ namespace TermProject.UserCreateLogin
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Random q = new Random();
+                recoverykey = q.Next(10000);
 
+                // hide this later
+                lblWarning.Text = recoverykey.ToString();
+            }
         }
 
         protected void btnCreateUser_Click(object sender, EventArgs e)
         {
+            if (recoverykey.ToString() == txtBoxCode.Text &&
+                txtBoxUsername.Text != "" &&
+                txtBoxFullName.Text != "" &&
+                txtBoxPassword.Text != "" &&
+                txtBoxEmail.Text != "" &&
+                txtBoxSecurityQuestion1.Text != "" &&
+                txtBoxSecurityQuestion2.Text != "" &&
+                txtBoxSecurityQuestion3.Text != "")
+            {
+                // creates the account
+                String username = txtBoxUsername.Text;
+                String fullname = txtBoxFullName.Text;
+                String email = txtBoxEmail.Text;
+                String usertype = ddlUserType.SelectedValue.ToString();
+                String question1 = txtBoxSecurityQuestion1.Text;
+                String question2 = txtBoxSecurityQuestion2.Text;
+                String question3 = txtBoxSecurityQuestion3.Text;
+
+                String password = sha256(txtBoxPassword.Text);
+
+                StoredProceduresClass.CreateUser(username, fullname, email, password, usertype, question1, question2, question3);
+                lblWarning.Text = "Account Created";
+
+                HttpCookie cookie = new HttpCookie("AgentAddSeller");
+
+                cookie.Values["username"] = username;
+                cookie.Values["name"] = fullname;
+                cookie.Expires = DateTime.Now.AddDays(30);
+                Response.Cookies.Add(cookie);
+
+                // add a user agent here
+                Response.Redirect("../Sellers/SellersUserCreation.aspx");
+            }
+            else
+            {
+                // display error here later
+                lblWarning.Text = "You have an error somewhere. FIX IT!!!!";
+            }
 
         }
     }
